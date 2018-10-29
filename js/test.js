59859fe418e0b4f84957263827ac89b2d0533b3b
gayatri P <gaya3p13@gmail.com>
2018-10-29 13:05:33 +0530

    fixes on test.js

--- js/test.js
+++ js/test.js
@@ -36,39 +36,70 @@ const values = {
     js: 35
 }
 
-for (const lang of Object.keys(values)) {
-    let skill = anime.timeline()
-    let perc = values[lang]
-    let val = (perc / 100) * 280
+let arrow = anime.timeline() 
 
-    skill
-        .add({
-            targets: `.skill-${lang} .skill-path`,
-            d: [
-                { value: `M 10 10 L 10 10 L 290 10` },
-                { value: `M 10 10 L ${val} 45 L 290 10` }
-            ],
-            easing: [0.55, 0.085, 0.68, 0.53],
-            duration: perc*25
-        })
-        .add({
-            targets:  `.skill-${lang} .skill-path`,
-            d: [
-                { value: `M 10 10 L ${val} 50 L 290 10` },
-                { value: `M 10 10 L ${val} 40 L 290 10` },
-                { value: `M 10 10 L ${val} 50 L 290 10` },
-                { value: `M 10 10 L ${val} 43 L 290 10` },
-                { value: `M 10 10 L ${val} 53 L 290 10` },
-                { value: `M 10 10 L ${val} 51 L 290 10` },
-                { value: `M 10 10 L ${val} 50 L 290 10` }
-            ],
-            easing: [0, 0, .58, 1],
-            duration: perc*25 - 200,
-        })
+const fallDown = _ => {
+    for (const lang of Object.keys(values)) {
+        let skill = anime.timeline()
+        let perc = values[lang]
+        let val = (perc / 100) * 280
+        let dur = perc < 40 ? perc * 50 : perc * 25
+        let delay = perc < 40 ? 200 : 0
+        skill
+            .add({
+                targets: `.skill-${lang} .skill-path`,
+                d: [
+                    { value: `M 10 10 L 10 10 L 290 10` },
+                    { value: `M 10 10 L ${val} 45 L 290 10` }
+                ],
+                easing: [0.55, 0.085, 0.68, 0.53],
+                duration: dur,
+                delay: delay
+            })
+            .add({
+                targets: `.skill-${lang} .skill-path`,
+                d: [
+                    { value: `M 10 10 L ${val} 50 L 290 10` },
+                    { value: `M 10 10 L ${val} 40 L 290 10` },
+                    { value: `M 10 10 L ${val} 50 L 290 10` },
+                    { value: `M 10 10 L ${val} 43 L 290 10` },
+                    { value: `M 10 10 L ${val} 53 L 290 10` },
+                    { value: `M 10 10 L ${val} 51 L 290 10` },
+                    { value: `M 10 10 L ${val} 50 L 290 10` }
+                ],
+                easing: [0, 0, .58, 1],
+                duration: dur + 200,
+                delay: delay
+            })
+    }
 }
+// fallDown()
 
-// const btnOpen = document.querySelector('#cloud_word_1')
-// console.log(btnOpen)
+
+arrow
+    .add({
+        targets: '.arrow',
+        duration: 1600,
+        left: '224px',
+        top: '50px',
+        easing: [0.55, 0.085, 0.68, 0.53],
+        delay: 800
+    })
+    /*.add ({
+        targets: '.arrow',
+        duration: 2200,
+        easing: [0, 0, .58, 1],
+        transform: [
+            {value: 'translateY(0px) rotate(0deg)'},
+            {value: 'translateY(-10px) rotate(150deg)'},
+            {value: 'translateY(10px) rotate(20deg)'},
+            {value: 'translateY(-7px) rotate(165deg)'},
+            {value: 'translateY(3px) rotate(170deg)'},
+            {value: 'translateY(1px) rotate(175deg)'},
+            {value: 'translateY(0px) rotate(179deg)'}
+        ]
+    })*/
+
 const btnClose = document.querySelector('#close')
 const popup = document.querySelector('.popup')
 const popupContent = popup.querySelector('.popup-content')
@@ -81,7 +112,6 @@ const common = {
 }
 
 const openPopup = _ => {
-    console.log('open clicked')
     popup.classList.add('active')
     backgroundSVG.style.zIndex = '2'
     anime({
@@ -91,6 +121,7 @@ const openPopup = _ => {
             { value: '200,100 0,100 0,0 200,0' }
         ]
     })
+    fallDown()
 }
 
 btnClose.addEventListener('click', _ => {
